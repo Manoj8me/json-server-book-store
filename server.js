76800d106eacf909d9dd32712b2f68a2ed95e40b
Manoj <manoj8me@gmail.com>
2024-11-11 01:28:13 +0530

    Update server.js to include json-server setup

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('books-db.json'); // Path to your JSON file
+const router = jsonServer.router("books-db.json"); // Path to your JSON file
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
